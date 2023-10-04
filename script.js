1258387593a794500b1826c71896f1f6620439d6
Esther Kamardine <kamardine.ossete@gmail.com>
2023-10-04 11:07:09 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const fetchAdvice = async() => {
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json()
+
+    console.log(data)
+
+    document.getElementById("number").innerHTML = data.slip.id 
+    document.getElementById("advice").innerHTML = data.slip.advice
+}
+
+fetchAdvice()
+
+
